74e3819d7150f92f6ea2f359e04e0594788f1953
Andrew Hong <honga2@carleton.edu>
2025-03-01 17:10:02 -0600

    some foundational stuff to test bot

--- bart-bot/responses.py
+++ bart-bot/responses.py
@@ -1,2 +1,11 @@
+from random import choice, randint
+
 def get_response(user_input: str) -> str:
-    raise NotImplementedError()
+    lowered: str = user_input.lower()
+
+    if lowered == '':
+        return 'Cat got ya tongue?'
+    else:
+        # return 'Deep Alan is in progress... Please check in later.'
+        return '0_0'
+
