d0326d8947c807435b7916a9cb5f76a582485662
Andrew Hong <honga2@carleton.edu>
2025-03-01 00:36:32 -0600

    add pipenv stuff

---
+++ bart-bot/bot.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+from discord import Intents, Client, Message
+from responses import get_response
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+print(TOKEN)
